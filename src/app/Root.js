9bcad17ab8ff47e3517eb41e5cb569abb808143c
Tadeusz Dudek <tadeusz.dudek@vml.com>
2017-04-14 11:53:40 +0200

    Add styles support.

---
+++ src/app/Root.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Loader from './components/loader/'
+import Loader2 from './components/loader2/'
+
+const Root = () => (
+    <div>Hello React Hot Loader!
+        <Loader/>
+        <Loader2/>
+    </div>
+)
+
+export default Root
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,33 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  devtool: 'source-map',
-  entry: {
-    'app': [
-      'babel-polyfill',
-      'react-hot-loader/patch',
-      './src/index'
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
-  }
+    devtool: 'source-map',
+    entry: {
+        'app': [
+            'babel-polyfill',
+            'react-hot-loader/patch',
+            './src/index'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.s?css$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader?modules&importLoaders=1',
+                    'postcss-loader'
+                ]
+            }
+        ],
+    },
 }
